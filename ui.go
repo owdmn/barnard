91a4580281fcff00abfea2109489653f52575969
Oliver Wiedemann <mail@oliver-wiedemann.net>
2025-10-20 12:49:25 +0200

    restore to original state, see if this still segfaults

--- ui.go
+++ ui.go
@@ -53,7 +53,6 @@ func (b *Barnard) OnVoiceToggle(ui *uiterm.Ui, key uiterm.Key) {
 		b.UiStatus.Fg = uiterm.ColorWhite | uiterm.AttrBold
 		b.UiStatus.Bg = uiterm.ColorRed
 		b.UiStatus.Text = "  Tx  "
-		b.Stream.StartSource()
 	}
 	ui.Refresh()
 }
